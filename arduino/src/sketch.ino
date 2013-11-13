915f09b8fa95f85325a20190206f1a0f3ee24449
Kris Jordan <krisjordan@gmail.com>
2013-11-13 18:45:12 -0500

    Initial commit.

--- arduino/src/sketch.ino
+++ arduino/src/sketch.ino
@@ -1,8 +1,14 @@
+#define LED_PIN 3
 
 void setup()
 {
+    pinMode(LED_PIN, OUTPUT);
 }
 
 void loop()
 {
+    digitalWrite(LED_PIN, HIGH);
+    delay(100);
+    digitalWrite(LED_PIN, LOW);
+    delay(900);
 }
